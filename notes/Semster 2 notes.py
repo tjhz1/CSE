05e8472a8f0cd1fae3e1e0d3df5dce57078813fe
tjhz1 <44381280+tjhz1@users.noreply.github.com>
2019-01-08 14:38:31 -0800

    Semster 2 notes

---
+++ notes/Semster 2 notes.py
@@ -0,0 +1,15 @@
+print("Hello world")
+# This is a single -line comment
+
+cars = 5
+driving = True
+
+print("I have %d cars" % cars)
+
+age = input("How old are you?")
+print("%s?? Really???" % age)
+
+colors = ["red","green","orange","blue","black"]
+colors.append("cyan")
+
+print(colors)
