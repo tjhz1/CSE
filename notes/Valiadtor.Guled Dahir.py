4683693b3fbf38f2263c4511630173c4a4b39c0a
tjhz1 <44381280+tjhz1@users.noreply.github.com>
2019-05-02 14:25:20 -0700

    validator

--- notes/Valiadtor.Guled Dahir.py
+++ notes/Valiadtor.Guled Dahir.py
@@ -6,21 +6,20 @@ def remove_num(string):
     return string[:15]
 
 
-remove_num(test_num[:15])
-
-print(remove_num(test_num))
-
-
 def reverse_it(string):
     return string[::-1]
 
 
-print(reverse_it(remove_num(test_num)))
-
-
 def validate(num: str):
-    for index in range(len(num)):
-        int_version = int(num[index])
+    cut_nums = remove_num(test_num[:15])
+    # print(cut_nums)
+    reversed_nums = reverse_it(cut_nums)
+    # print(reversed_nums)
+    list_nums = list(reversed_nums)
+    print(list_nums)
+    for index in range(len(list_nums)):
+        list_nums[index] = int(list_nums[index])
+    print(list_nums)
 
 
 print(validate(test_num))
